79d7133941686abfacd6054f196b799047d3716e
Gersane <g.segbeaya@hotmail.fr>
2024-05-28 14:45:30 +0200

    Add backend files for game and changes in morpion html

--- django/morpion/models.py
+++ django/morpion/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+class Match(models.Model):
+	player = models.ForeignKey(CustomUser, related_name='matches', on_delete=models.CASCADE)
+	alias = models.TextField(blank=True, max_length=15)
+	user_score = models.IntegerField(null=True, blank=True)
+	alias_score = models.IntegerField(null=True, blank=True)
+	winner = models.ForeignKey(CustomUser, related_name='matches_won', on_delete=models.CASCADE, null=True, blank=True)
+
+	def set_winner(self):
+		if self.user_score > self.alias_score:
+			self.winner = self.player
+			print(self.winner)
+		else:
+			print('No winner!')
+			self.winner = None
---
+++ django/morpion/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+		path("save-score/", views.save_score, name="save-score"),
+		path("create-match/", views.create_match, name="create_match"),
+		]
