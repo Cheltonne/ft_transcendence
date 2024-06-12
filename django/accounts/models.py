533cfb564cc5cf0e0168851b182c6539630147c2
Your Name <you@example.com>
2024-06-12 17:29:51 +0200

    modified User model with wins losses and friends list

--- django/accounts/models.py
+++ django/accounts/models.py
@@ -4,5 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
 	username = models.CharField(max_length=15, unique=True)
 	profile_picture = models.ImageField(upload_to='profile_pictures')
+	wins = models.IntegerField(default=0)
+	losses = models.IntegerField(default=0)
+	friends = models.ManyToManyField('self', blank=True)
 	def __str__(self):
 		return self.username
